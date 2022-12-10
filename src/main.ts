20c1fa94adfdd04ca5467311b19a72ff650dd536
Thales <thalestr@outlook.com>
2022-12-10 16:16:34 -0300

    feat: add v-calendar

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,14 @@ import * as mdijs from '@mdi/js'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import 'v-calendar/dist/style.css';
+
 import BaseCard from './components/ui/BaseCard.vue'
 import BaseButton from './components/ui/BaseButton.vue'
+import VCalendar from 'v-calendar';
 
 const app = createApp(App)
+app.use(VCalendar, {})
 
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,5 +1,30 @@
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import ProfileCard from '../components/Cards/ProfileCard.vue';
+
+const teste = new Date();
+
+const today = [
+  {
+    key: 'today',
+    highlight: true,
+    dates: new Date(),
+  },
+]
+
+</script>
+
 <template>
   <div class="dashboard-view--container">
-    dashboard view
+    <profile-card />
+    <base-card>
+      <v-calendar :attributes="today" />
+    </base-card>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.dashboard-view--container {
+  display: flex;
+}
+</style>
